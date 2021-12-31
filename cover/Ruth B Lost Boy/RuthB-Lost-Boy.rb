eeb1874118469514146ad6205a9b293b97d78418
samuelowino <samuelowino43@yahoo.com>
2022-01-01 02:22:58 +0300

    Lost B, main loop

---
+++ cover/Ruth B Lost Boy/RuthB-Lost-Boy.rb
@@ -0,0 +1,30 @@
+# Ruth B, Lost Boy
+
+use_bpm 124
+
+live_loop :intro do
+  #Entry Piono Verse
+  play :g, sustain: 1
+  play :e, sustain: 1
+  play :b, sustain: 1
+  play :g, sustain: 1, amp:1
+  play :e, sustain: 1, amp:1
+  play :b, sustain: 1, amp:1
+  sleep 4
+  play :g, sustain: 1, amp: 0.5
+  play :e, sustain: 1, amp: 0.5
+  play :a, sustain: 1
+  play :b, sustain: 1
+  sleep 4
+  play :c, sustain: 1
+  play :e, sustain: 1
+  play :g, sustain: 1
+  play :e, sustain: 1
+  play :g, sustain: 1
+  sleep 4
+  play :c, sustain: 1
+  play :c, sustain: 1
+  play :b, sustain: 0.5
+  play :d, sustain: 1
+  sleep 3.5
+end

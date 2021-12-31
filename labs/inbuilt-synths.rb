ca0189ad08413f042af4cfb17d4c1f287c5d3083
samuelowino <samuelowino43@yahoo.com>
2021-12-31 17:43:42 +0300

    Inbuilt synths

---
+++ labs/inbuilt-synths.rb
@@ -0,0 +1,62 @@
+sample :ambi_lunar_land, release: 2
+use_synth :saw
+
+play 38
+
+sleep 0.5
+
+play 35, amp: 2
+
+sleep 0.5
+
+use_synth :prophet
+play 38
+sleep 0.5
+play 40, amp: 2, release: 2
+sleep 0.5
+
+sample :ambi_lunar_land, release: 2
+use_synth :saw
+play 38
+
+sleep 0.5
+
+play 50, amp: 2
+
+use_synth :prophet
+play 38, amp: 2
+sleep 0.5
+play 50, amp: 2
+sleep 0.5
+play 62, amp: 2
+
+sample :ambi_lunar_land, release: 2
+use_synth :saw
+sleep 1
+play 38, amp: 2, release: 2
+sleep 0.5
+play 50, amp: 2
+sleep 0.5
+play 65, amp: 2
+
+sample :ambi_lunar_land, release: 2
+use_synth :prophet
+sleep 1
+play 38, amp: 2, release: 2
+sleep 0.5
+play 50, amp: 2
+sleep 0.5
+play 72, amp: 2
+
+sample :ambi_lunar_land, release: 2
+use_synth :saw
+sleep 1
+play 38, amp: 2, release: 2
+sleep 0.5
+play 50, amp: 2
+sleep 0.5
+play 62, amp: 2
+use_synth :tb303
+play 70, amp:2, release: 2
+use_synth :dsaw
+play 50

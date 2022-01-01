80bdf16f985671bcc24edd35902822c0746e1606
samuelowino <samuelowino43@yahoo.com>
2022-01-01 20:38:27 +0300

    Summarise chord placement

--- cover/Ruth B Lost Boy/RuthB-Lost-Boy-EDM.rb
+++ cover/Ruth B Lost Boy/RuthB-Lost-Boy-EDM.rb
@@ -1,60 +1,29 @@
 # Ruth B, Lost Boy
-
 use_bpm 124
 
 2.times do
   #Entry Piono Verse
   use_synth :tech_saws
-  play :g, sustain: 1, amp: 0.1
-  play :e, sustain: 1, amp: 0.1
-  play :b, sustain: 1, amp: 0.1
-  play :g, sustain: 1, amp: 0.1
-  play :e, sustain: 1, amp: 0.1
-  play :b, sustain: 1, amp: 0.1
+  play [:G, :E, :B], sustain: 1
   sleep 4
-  play :g, sustain: 1, amp: 0.4
-  play :e, sustain: 1, amp: 0.4
-  play :a, sustain: 1, amp: 0.4
-  play :b, sustain: 1, amp: 0.4
+  play [:G, :E, :A, :B], sustain: 1, amp: 0.4
   sleep 4
-  play :c, sustain: 1, amp: 0.7
-  play :e, sustain: 1, amp: 0.7
-  play :g, sustain: 1, amp: 0.7
-  play :e, sustain: 1, amp: 0.7
-  play :g, sustain: 1, amp: 0.7
+  play [:C, :E, :G], sustain: 1, amp: 0.7
   sleep 4
-  play :c, sustain: 1, amp: 0.1
-  play :c, sustain: 1, amp: 0.1
-  play :b, sustain: 0.5, amp: 0.1
-  play :d, sustain: 1, amp: 0.1
+  play [:C, :B, :D], sustain: 1, amp: 0.1
   sleep 4
 end
 
 live_loop :intro do
   #Entry Piono Verse
   use_synth :tech_saws
-  play :g, sustain: 1
-  play :e, sustain: 1
-  play :b, sustain: 1
-  play :g, sustain: 1, amp:1
-  play :e, sustain: 1, amp:1
-  play :b, sustain: 1, amp:1
+  play [:G, :E, :B], sustain: 1, amp: 1
   sleep 4
-  play :g, sustain: 1, amp: 0.5
-  play :e, sustain: 1, amp: 0.5
-  play :a, sustain: 1
-  play :b, sustain: 1
+  play [:G, :E, :A, :B], sustain: 1, amp: 0.5
   sleep 4
-  play :c, sustain: 1
-  play :e, sustain: 1
-  play :g, sustain: 1
-  play :e, sustain: 1
-  play :g, sustain: 1
+  play [:C, :E, :G], sustain: 1, amp: 0.7
   sleep 4
-  play :c, sustain: 1
-  play :c, sustain: 1
-  play :b, sustain: 0.5
-  play :d, sustain: 1
+  play [:C, :B, :D], sustain: 1, amp: 0.1
   sleep 4
 end
 
@@ -69,24 +38,3 @@ live_loop :drums do
   sleep 1
 end
 
-
-live_loop :snare do
-  1.times do
-    
-    16.times do
-      sample :drum_cymbal_pedal, start: 0.05, finish: 0.4, rate: 3, amp: 0.5 + rrand(-0.1, 0.1)
-      sleep 0.125
-    end
-    
-    4.times do
-      sample :drum_cymbal_pedal, start: 0.05, finish: 0.6, rate: 3, amp: 0.5 + rrand(-0.1, 0.1)
-      sleep 0.25
-    end
-    
-    16.times do
-      sample :drum_cymbal_pedal, start: 0.1, finish: 0.3, rate: 3, amp: 0.5 + rrand(-0.1, 0.1)
-      sleep 0.0625
-    end
-  end
-  sleep 24
-end
